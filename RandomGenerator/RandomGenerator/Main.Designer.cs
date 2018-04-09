b2310caf94dfb6806b8caf1c6c12f5fbf733acdf
Alexander <alexandersinzig@yahoo.de>
2018-04-09 14:12:49 +0100

    started working on 'result'-button
    for-loop to find instances of boxes

--- RandomGenerator/RandomGenerator/Main.Designer.cs
+++ RandomGenerator/RandomGenerator/Main.Designer.cs
@@ -88,6 +88,7 @@
             this.ButtonResult.TabIndex = 4;
             this.ButtonResult.Text = "Result";
             this.ButtonResult.UseVisualStyleBackColor = true;
+            this.ButtonResult.Click += new System.EventHandler(this.ButtonResult_Click);
             // 
             // FormMain
             // 
--- RandomGenerator/RandomGenerator/Main.cs
+++ RandomGenerator/RandomGenerator/Main.cs
@@ -12,9 +12,42 @@ namespace RandomGenerator
 {
     public partial class FormMain : Form
     {
+
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void ButtonResult_Click(object sender, EventArgs e)
+        {
+            //for the number of different diceTypes
+            int diceTypeCounter = 0;
+            //for the number of dices of one specific type
+            int dicesOfTypeXCounter = 0;
+
+            //iterate through the types
+            for (int i = 0; i > diceTypeCounter; i++)
+            {
+                //find instances of the boxes and save them as variables
+                if (this.Controls.ContainsKey("ComboBoxDiceSides" + i.ToString()))
+                {
+                    ComboBox diceTypeBox = this.Controls["ComboBoxDiceSides" + i.ToString()] as ComboBox;
+                }
+                if (this.Controls.ContainsKey("TextBoxDiceAmount" + i.ToString()))
+                {
+                    TextBox diceAmountBox = this.Controls["TextBoxDiceAmount" + i.ToString()] as TextBox;
+                }
+                if (this.Controls.ContainsKey("ListboxResultOutput" + i.ToString()))
+                {
+                    ListBox ResultBox = this.Controls["ListboxResultOutput" + i.ToString()] as ListBox;
+                }
+
+                //combo-box[i].wert auslesen
+                //text-box[i].wert auslesen
+                //listbox[i] aufrufen und in Schleife mit der Länge von text-box[i].wert einen Wert zwischen 1 und combo-box[i].wert eintragen
+            }
+
+            ListboxResultOutput.Items[0] = 
         }
     }
 }
