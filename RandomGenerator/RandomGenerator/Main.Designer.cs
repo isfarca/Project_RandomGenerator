e0b55565de65f7647d3bbc4dbafe6422e6a1dbc2
Alexander <alexandersinzig@yahoo.de>
2018-04-17 10:29:04 +0200

    Rescaling
    should have been done in the design-branch but now it's too late

--- RandomGenerator/RandomGenerator/Main.Designer.cs
+++ RandomGenerator/RandomGenerator/Main.Designer.cs
@@ -45,6 +45,11 @@
             this.EnterOptionTextbox = new System.Windows.Forms.TextBox();
             this.ButtonRemoveDiceType = new System.Windows.Forms.Button();
             this.Output = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ComboBoxDiceSides
@@ -58,7 +63,7 @@
             "6",
             "12",
             "20"});
-            this.ComboBoxDiceSides.Location = new System.Drawing.Point(12, 12);
+            this.ComboBoxDiceSides.Location = new System.Drawing.Point(12, 58);
             this.ComboBoxDiceSides.Name = "ComboBoxDiceSides";
             this.ComboBoxDiceSides.Size = new System.Drawing.Size(121, 28);
             this.ComboBoxDiceSides.TabIndex = 0;
@@ -72,7 +77,7 @@
             this.TextboxDiceAmount.BackColor = System.Drawing.Color.PeachPuff;
             this.TextboxDiceAmount.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.TextboxDiceAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TextboxDiceAmount.Location = new System.Drawing.Point(139, 12);
+            this.TextboxDiceAmount.Location = new System.Drawing.Point(139, 58);
             this.TextboxDiceAmount.Name = "TextboxDiceAmount";
             this.TextboxDiceAmount.Size = new System.Drawing.Size(117, 25);
             this.TextboxDiceAmount.TabIndex = 1;
@@ -87,7 +92,7 @@
             this.ListboxResultOutput.FormattingEnabled = true;
             this.ListboxResultOutput.HorizontalScrollbar = true;
             this.ListboxResultOutput.ItemHeight = 25;
-            this.ListboxResultOutput.Location = new System.Drawing.Point(262, 12);
+            this.ListboxResultOutput.Location = new System.Drawing.Point(262, 58);
             this.ListboxResultOutput.Name = "ListboxResultOutput";
             this.ListboxResultOutput.Size = new System.Drawing.Size(416, 25);
             this.ListboxResultOutput.TabIndex = 2;
@@ -100,7 +105,7 @@
             this.ButtonAddDiceType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ButtonAddDiceType.ForeColor = System.Drawing.Color.PeachPuff;
             this.ButtonAddDiceType.Image = global::RandomGenerator.Properties.Resources._8357a3f1824f652;
-            this.ButtonAddDiceType.Location = new System.Drawing.Point(13, 47);
+            this.ButtonAddDiceType.Location = new System.Drawing.Point(13, 93);
             this.ButtonAddDiceType.Name = "ButtonAddDiceType";
             this.ButtonAddDiceType.Size = new System.Drawing.Size(120, 27);
             this.ButtonAddDiceType.TabIndex = 3;
@@ -118,7 +123,7 @@
             this.ButtonResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ButtonResult.ForeColor = System.Drawing.Color.PeachPuff;
             this.ButtonResult.Image = global::RandomGenerator.Properties.Resources._8357a3f1824f652;
-            this.ButtonResult.Location = new System.Drawing.Point(562, 47);
+            this.ButtonResult.Location = new System.Drawing.Point(562, 93);
             this.ButtonResult.Name = "ButtonResult";
             this.ButtonResult.Size = new System.Drawing.Size(116, 27);
             this.ButtonResult.TabIndex = 4;
@@ -132,7 +137,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Maroon;
-            this.label1.Location = new System.Drawing.Point(133, 364);
+            this.label1.Location = new System.Drawing.Point(133, 316);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(201, 31);
             this.label1.TabIndex = 6;
@@ -208,7 +213,7 @@
             this.OptionsListBox.ItemHeight = 16;
             this.OptionsListBox.Location = new System.Drawing.Point(160, 415);
             this.OptionsListBox.Name = "OptionsListBox";
-            this.OptionsListBox.Size = new System.Drawing.Size(120, 32);
+            this.OptionsListBox.Size = new System.Drawing.Size(113, 32);
             this.OptionsListBox.TabIndex = 11;
             this.OptionsListBox.UseWaitCursor = true;
             // 
@@ -239,9 +244,9 @@
             this.EnterOptionTextbox.BackColor = System.Drawing.Color.PeachPuff;
             this.EnterOptionTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.EnterOptionTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EnterOptionTextbox.Location = new System.Drawing.Point(21, 414);
+            this.EnterOptionTextbox.Location = new System.Drawing.Point(19, 414);
             this.EnterOptionTextbox.Name = "EnterOptionTextbox";
-            this.EnterOptionTextbox.Size = new System.Drawing.Size(100, 24);
+            this.EnterOptionTextbox.Size = new System.Drawing.Size(114, 24);
             this.EnterOptionTextbox.TabIndex = 14;
             // 
             // ButtonRemoveDiceType
@@ -252,7 +257,7 @@
             this.ButtonRemoveDiceType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ButtonRemoveDiceType.ForeColor = System.Drawing.Color.PeachPuff;
             this.ButtonRemoveDiceType.Image = global::RandomGenerator.Properties.Resources._8357a3f1824f652;
-            this.ButtonRemoveDiceType.Location = new System.Drawing.Point(139, 47);
+            this.ButtonRemoveDiceType.Location = new System.Drawing.Point(139, 93);
             this.ButtonRemoveDiceType.Name = "ButtonRemoveDiceType";
             this.ButtonRemoveDiceType.Size = new System.Drawing.Size(120, 27);
             this.ButtonRemoveDiceType.TabIndex = 15;
@@ -264,12 +269,67 @@
             // 
             this.Output.AutoSize = true;
             this.Output.BackColor = System.Drawing.Color.Transparent;
-            this.Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Output.Location = new System.Drawing.Point(363, 364);
+            this.Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output.Location = new System.Drawing.Point(401, 15);
             this.Output.Name = "Output";
-            this.Output.Size = new System.Drawing.Size(90, 31);
+            this.Output.Size = new System.Drawing.Size(77, 26);
             this.Output.TabIndex = 5;
-            this.Output.Text = "output";
+            this.Output.Text = "Output";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(144, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 52);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Amount of\r\n    dices";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 26);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Dice type";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 370);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 26);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Enter options";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(164, 370);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 26);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Option List";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(344, 370);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 26);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Go!";
             // 
             // FormMain
             // 
@@ -277,6 +337,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::RandomGenerator.Properties.Resources.mystic_background_by_lirdrak_d712xh4;
             this.ClientSize = new System.Drawing.Size(690, 501);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.ButtonRemoveDiceType);
             this.Controls.Add(this.EnterOptionTextbox);
             this.Controls.Add(this.CoinflipResultLabel);
@@ -321,6 +386,11 @@
         private System.Windows.Forms.TextBox EnterOptionTextbox;
         private System.Windows.Forms.Button ButtonRemoveDiceType;
         private System.Windows.Forms.Label Output;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
